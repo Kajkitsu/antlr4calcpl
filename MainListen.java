539bea7ca33c1d89d6c93763dfc110fc1827ffed
Norbert Waszkowiak <norbert.waszkowiak@gmail.com>
2022-11-15 14:06:35 +0100

    update

--- MainListen.java
+++ MainListen.java
@@ -1,57 +1,49 @@
+import java.util.Stack;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.util.Stack;
 
-public class MainListen {
-    private static class Listener extends CalculatorBaseListener {
+public class MainListen extends CalculatorBaseListener {
 
-        private final Stack<Integer> stack = new Stack<>();
+    private final Stack<Integer> stack = new Stack<>();
 
-        public int getResult() {
-            return stack.peek();
-        }
-
-        @Override
-        public void exitMulDiv(CalculatorParser.MulDivContext ctx) {
-            int right = stack.pop();
-            int left = stack.pop();
-            int result;
-            if (ctx.op.getType() == CalculatorParser.TIMES) {
-                result = left * right;
-            } else {
-                result = left / right;
-            }
-            stack.push(result);
-        }
-
-        @Override
-        public void exitAddSub(CalculatorParser.AddSubContext ctx) {
-            int right = stack.pop();
-            int left = stack.pop();
-            int result;
-            if (ctx.op.getType() == CalculatorParser.PLUS) {
-                result = left + right;
-            } else {
-                result = left - right;
-            }
-            stack.push(result);
-        }
-
-        @Override
-        public void exitInt(CalculatorParser.IntContext ctx) {
-            stack.push(Integer.valueOf(ctx.INT().getText()));
-        }
+    public Integer getResult() {
+        return stack.peek();
     }
 
+    @Override
+    public void exitAddSub(CalculatorParser.AddSubContext ctx) {
+        int right = stack.pop();
+        int left = stack.pop();
+        int result;
+        if (ctx.op.getType() == CalculatorParser.PLUS) {
+            result = left + right;
+        } else {
+            result = left - right;
+        }
+        stack.push(result);
+    }
+
+    @Override
+    public void exitInt(CalculatorParser.IntContext ctx) {
+        stack.push(Integer.valueOf(ctx.INT().getText()));
+    }
+
+    @Override
+    public void exitMulDiv(CalculatorParser.MulDivContext ctx) {
+        int right = stack.pop();
+        int left = stack.pop();
+        int result;
+        if (ctx.op.getType() == CalculatorParser.TIMES) {
+            result = left * right;
+        } else {
+            result = left / right;
+        }
+        stack.push(result);
+    }
+    
     public static void main(String[] args) throws Exception {
-        String inputFile = null;
-        if ( args.length>0 ) inputFile = args[0];
-        InputStream is = System.in;
-        if ( inputFile!=null ) is = new FileInputStream(inputFile);
-        CharStream charStreams = CharStreams.fromStream(is);
+        CharStream charStreams = CharStreams.fromFileName​("example.txt");
         CalculatorLexer lexer = new CalculatorLexer(charStreams);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         //System.out.println(tokens.getText());
@@ -60,8 +52,10 @@ public class MainListen {
         ParseTree tree = parser.expr(); // parse
 
         ParseTreeWalker walker = new ParseTreeWalker();
-        Listener listener = new Listener();
+        MainListen listener = new MainListen();
         walker.walk(listener, tree);
         System.out.println(listener.getResult());
     }
+    
+
 }
